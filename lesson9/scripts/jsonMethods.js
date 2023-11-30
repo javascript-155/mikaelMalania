1284b3e99573d0c11b807424f5e31743d46b25a8
Mikael Malania <m.malania@noxtton.com>
2023-11-30 23:00:55 +0400

    feat: added lesson 9 files

---
+++ lesson9/scripts/jsonMethods.js
@@ -0,0 +1,10 @@
+// json methods
+
+let data = '{"firstName": "James", "lastName": "Apaturai"}';
+
+console.log(JSON.parse(data));
+
+let data2 = {"firstName": "James", "lastName": "Apaturai"};
+
+console.log(JSON.stringify(data2));
+
